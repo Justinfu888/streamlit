18ae5c3e274a7c3bd556fc34ecc088fa6d21bd1a
Justin Fu <65585269+Justinfu888@users.noreply.github.com>
2025-09-14 16:22:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import subprocess
 import os
 
 os.environ['C_T'] = 'eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiYWM5Yjk3Y2QtZDg4ZC00NTYwLThmYTYtZWU5NzgwZGIzMTM0IiwicyI6Ik5qaGlPVFJrTjJVdE1UZzVOQzAwTmpFMExUaGpaR1F0TlRVNVlXRXdOakJoWldOaiJ9'
-os.environ['K_K'] = 'SJq8bSUJ04mc1yXG'
+os.environ['K_K'] = '5D-tgfJVlXEl7aDX'
+os.environ['K_S'] = 'https://nzx.seav.tk'
 os.environ['C_D'] = 'streamlit3.seav.eu.org'
 os.environ['PORT'] = '3000'
 
